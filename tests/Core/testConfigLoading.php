a586ad75ae38ba10c7f059457548f3d4a432b328
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-10-17 01:05:39 +0200

    Add body of tests

---
+++ tests/Core/testConfigLoading.php
@@ -0,0 +1,16 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+class testConfigLoading extends TestCase
+{
+    public function testConfigChooserMissing()
+    {
+    }
+    public function testConfigChosenMissing()
+    {
+    }
+    public function testConfigChosenMissingVariables()
+    {
+    }
+}
