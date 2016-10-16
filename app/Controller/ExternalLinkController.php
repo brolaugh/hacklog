e03bdc267fb8d6231a920305b501fd4078075bca
Hannes "Brolaugh" Kindströmmer <me@brolaugh.com>
2016-10-16 02:17:54 +0200

    Add redirect, link warning class

---
+++ app/Controller/ExternalLinkController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Controller;
+
+
+class ExternalLinkController
+{
+    /**
+     * Tells the client that the link has not been validated or verified by the blog portal staff and is therefor
+     * marked unsafe
+     * Gives a link to the actual page
+     */
+    public function index(){
+
+    }
+}
---
+++ app/Core/Redirect.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace HackLog\Core;
+
+
+class Redirect
+{
+    /**
+     * Redirects to an
+     * @param string $location
+     */
+    public static function to(string $location){
+        header("Location:/$location");
+        exit();
+    }
+
+    /**
+     * Redirects to a page that tells the user of the dangers of visiting unknown page
+     * Page in question: /externallink
+     *
+     * @param string $adress
+     */
+    public static function toExternal(string $adress){
+
+    }
+    /**
+     * Same as method toExternal but without the protecting layer
+     *
+     * @param string $adress
+     */
+    public static function toExternalReal(string $adress){
+        header("Location: $adress");
+        exit();
+    }
+}
